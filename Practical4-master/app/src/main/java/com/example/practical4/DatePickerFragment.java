7785a4fb5366c88d1ad388c0d988d69d160b15dc
Chew Wei Sheng <chewws-wm17@student.tarc.edu.my>
2019-06-24 12:25:56 +0800

    commit

--- Practical4-master/app/src/main/java/com/example/practical4/DatePickerFragment.java
+++ Practical4-master/app/src/main/java/com/example/practical4/DatePickerFragment.java
@@ -23,25 +23,20 @@ import java.util.Calendar;
 public class DatePickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener {
 
-    public void onDateSet(DatePicker datePicker,
-                          int year, int month, int day)
+    public void onDateSet(DatePicker datePicker, int year, int month, int day)
     {
-        // Set the activity to the Main Activity.
         MainActivity activity = (MainActivity) getActivity();
-        // Invoke Main Activity's processDatePickerResult() method.
         activity.processDatePickerResult(year, month, day);
     }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker.
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-// Create a new instance of DatePickerDialog and return it.
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
--- Practical4-master/app/src/main/java/com/example/practical4/MainActivity.java
+++ Practical4-master/app/src/main/java/com/example/practical4/MainActivity.java
@@ -80,25 +80,19 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_settings:
                 displayToast(getString(R.string.action_settings_message));
-                // User chose the "Settings" item, show the app settings UI...
                 return true;
 
             case R.id.action_favorite:
                 displayToast(getString(R.string.action_favorite_message));
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
                 return true;
 
             default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
         }
     }
     public void displayToast(String message) {
-        Toast.makeText(getApplicationContext(), message,
-                Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
  public void launchSecondActivity(View view){
     Intent intent = new Intent(this, SecondActivity.class);
@@ -113,11 +107,9 @@ public class MainActivity extends AppCompatActivity {
         String month_string = Integer.toString(month+1);
         String day_string = Integer.toString(day);
         String year_string = Integer.toString(year);
-        String dateMessage = (month_string +
-                "/" + day_string + "/" + year_string);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
 
-        Toast.makeText(this, "Date: " + dateMessage,
-                Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
     }
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
@@ -127,8 +119,7 @@ public class MainActivity extends AppCompatActivity {
     public void processTimePickerResult(int hourOfDay, int minute) {
         String hourOfDay_string = Integer.toString(hourOfDay);
         String minute_string = Integer.toString(minute);
-        String timeMessage = (hourOfDay +
-                ":" + minute);
+        String timeMessage = (hourOfDay + ":" + minute);
 
         Toast.makeText(this, "Time: " + timeMessage,
                 Toast.LENGTH_SHORT).show();
@@ -138,27 +129,20 @@ public class MainActivity extends AppCompatActivity {
     public void onClickShowAlert(View view) {
         AlertDialog.Builder myAlertBuilder = new
                 AlertDialog.Builder(MainActivity.this);
-        // Set the dialog title and message.
-        // Set the dialog title and message.
+
         myAlertBuilder.setTitle("Alert");
         myAlertBuilder.setMessage("Click OK to continue, or Cancel to stop:");
-// Add the dialog buttons.
 
-        // Add the dialog buttons.
-        myAlertBuilder.setPositiveButton("OK", new
-                DialogInterface.OnClickListener() {
+        myAlertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        // User clicked OK button.
-                        Toast.makeText(getApplicationContext(),"You Pressed OK",
-                                Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(getApplicationContext(),"OK", Toast.LENGTH_SHORT).show();
                     }
                 });
-        myAlertBuilder.setNegativeButton("Cancel", new
-                DialogInterface.OnClickListener() {
+        myAlertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        // User cancelled the dialog.
-                        Toast.makeText(getApplicationContext(), "You Pressed Cancel",
-                                Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
                     }
                 });
 
